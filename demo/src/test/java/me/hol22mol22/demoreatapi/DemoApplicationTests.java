7a791ccae318bfc1c1edc562b03db9003bf6a3a7
merong245 <37497183+merong245@users.noreply.github.com>
2022-05-18 02:35:16 +0900

    init

---
+++ demo/src/test/java/me/hol22mol22/demoreatapi/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hol22mol22.demoreatapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
