5d9259a99fadb2b0c3d368bbb8fba6f0a2b8fb70
이준혁 <merong245@gmail.com>
2022-07-08 19:09:03 +0900

    스프링 시큐리티 설정
    스프링 시큐리티가 의존성에 들어가면 스프링 부트는 스프링 시큐리티용 자동 설정을 적용하게 되고, 모든 요청은 인증을 필요로 하게됨
    
    따라서 스프링 시큐리티 config 설정

---
+++ demo/src/main/java/me/hol22mol22/demorestapi/configs/SecurityConfig.java
@@ -0,0 +1,52 @@
+package me.hol22mol22.demorestapi.configs;
+
+import me.hol22mol22.demorestapi.accounts.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+@Configuration // 빈 설정 파일이므로
+@EnableWebSecurity  // 우리가 정의하는 웹 시큐리티 설정으로 적용
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Bean
+    public TokenStore tokenStore(){
+        return new InMemoryTokenStore();
+    }
+
+    @Bean // 다른데서 참조 할 수 있도록 빈으로 노출
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(accountService)
+                .passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception{
+        web.ignoring().mvcMatchers("/docs/index.html");
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations()); // 기본 정적 리소스는 무시
+    }
+
+
+}
